6c6164992cac8b5135485b654f9e1a14b951ee9f
Duy Khang <benben211004@gmail.com>
2026-03-10 10:14:40 +0700

    feat: thêm chức năng tính tổng bằng python

---
+++ sum.py
@@ -0,0 +1,8 @@
+def tinh_tong(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    so1 = 10
+    so2 = 5
+    ket_qua = tinh_tong(so1, so2)
+    print(f"Tổng của {so1} và {so2} là: {ket_qua}")
